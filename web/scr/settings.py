cc98b7c95d8716280c1d9ab28f47e0cb396d29a7
bandirom <bandirom@ukr.net>
2020-05-05 22:39:30 +0300

    smth changed

--- web/scr/settings.py
+++ web/scr/settings.py
@@ -31,11 +31,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
-
     'defender',
 
-
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,9 +72,10 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
