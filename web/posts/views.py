622b0ed517a4d569c69c5b353270c5cb0d73cd0b
bandirom <bandirom@ukr.net>
2020-05-06 17:10:49 +0300

    Added Delete (cRuD) post function. Added Mixin class

--- web/posts/views.py
+++ web/posts/views.py
@@ -1,41 +1,73 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, RedirectView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, RedirectView, CreateView, DeleteView
 from .models import ArticleModel, Comment
+from .forms import ArticleCreateForm
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from .forms import CommentForm
 
 
-class ArticleListView(ListView):
-    queryset = ArticleModel.objects.all()  # <p>/<modelname>_list.html
-    template_name = 'posts/articles_list.html'
+class ArticleMixin(object):
     model = ArticleModel
-    paginate_by = 5
-    context_object_name = 'articles'
-    ordering = ['-timestamp']
-
-
-class ArticleDetailView(DetailView):
-    template_name = 'posts/article_detail.html'
     context_object_name = 'article'
 
     def get_object(self):
         slug = self.kwargs.get('slug')
-        return get_object_or_404(ArticleModel, slug=slug)
+        return get_object_or_404(self.model, slug=slug)
+
+
+class ArticleListView(ArticleMixin, ListView):
+    # queryset = ArticleModel.objects.all()  # <p>/<modelname>_list.html
+    template_name = 'posts/articles_list.html'
+    context_object_name = 'articles'
+    paginate_by = 5
+    ordering = ['-timestamp']
+
+
+class ArticleDeleteView(ArticleMixin, DeleteView):
+    template_name = 'posts/article_delete.html'
+
+    def get_success_url(self):
+        return reverse("articles:article-list")
+
+
+class ArticleDetailView(ArticleMixin, DetailView):
+    template_name = 'posts/article_detail.html'
 
     def get_context_data(self, **kwargs):
         article = self.get_object()
         last_comments = article.comment_set.order_by('-timestamp')[:10]
         data = super().get_context_data(**kwargs)
+        comment_form = CommentForm()
+        data['comment_form'] = comment_form
         data['last_comments'] = last_comments
         return data
+
+
+class ArticleCreateView(ArticleMixin, CreateView):
+    template_name = 'posts/create_update.html'
+    form_class = ArticleCreateForm
+
+    # def get_context_data(self, **kwargs):
+    #     print('get_context_data')
+    #     super().get_context_data(**kwargs)
+    #
+    # def get(self, request, *args, **kwargs):
+    #     print('get')
+    #     super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
 
 
 class LeaveComment(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         slug = self.kwargs.get('slug')
         article = get_object_or_404(ArticleModel, slug=slug)
-        article.comment_set.create(author=self.request.POST['name'], comment_text=self.request.POST['text'])
+        article.comment_set.create(author=self.request.POST['author'], comment_text=self.request.POST['comment_text'])
         return article.get_absolute_url()
 
 
