46f4640047c62ff124568e983865b32cc0266ea4
bandirom <bandirom@ukr.net>
2020-05-13 19:55:33 +0300

    changes

--- web/entrypoint.prod.sh
+++ web/entrypoint.prod.sh
@@ -2,7 +2,7 @@
 
 docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations
 docker-compose -f docker-compose.prod.yml exec web python manage.py migrate
-docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic
 docker-compose -f docker-compose.prod.yml exec web python manage.py createsuperuser
 
 exec "$@"
