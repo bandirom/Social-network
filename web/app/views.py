e0f90059fa4144139e57b1f1d7bf9b4720e8d6bc
bandirom <bandirom@ukr.net>
2020-06-24 01:33:38 +0300

    added custom 400, 500 error html

--- web/app/views.py
+++ web/app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from .models import Profile
+from django.template import RequestContext
+
+
+def custom_handler404(request, exception):
+    print("404 error")
+    context = {}
+    response = render(request, 'app/404.html', context=context)
+    response.status_code = 404
+    return response
+
+
+def custom_handler500(request):
+    print("500 error")
+    context = {}
+    response = render(request, 'app/500.html', context=context)
+    response.status_code = 500
+    return response
+
+
 
 
 def index(request):
